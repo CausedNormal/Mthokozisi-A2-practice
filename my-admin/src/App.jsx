a902ce7364e94affe4291d601b9fc7dae4e824ae
CausedNormal <mthokozisingcobo.ngcobo@gmail.com>
2025-11-09 18:09:09 +0000

    AuthorTable

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,6 +1,8 @@
-import { Admin, Resource, ListGuesser } from "react-admin"; // Took from src/admin/index.tsx from Marmalab
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin"; // Took from src/admin/index.tsx from Marmalab
 import { createTrailbaseProvider } from "./ra-trailbase"; // we typed it out ourselves
-import {AuthorList} from "./Author.jsx";                    // After making Author File imprt it to App 
+import {AuthorCreate, AuthorEdit, AuthorList, AuthorShow} from "./Author.jsx";                    // After making Author File imprt it to App , remember to import all of it.
+
+// Remember put {AuthorCreate, AuthorEdit, AuthorList, AuthorShow } into here as you go
 
 const TrailbaseURL = "https://special-adventure-g45wgj656g6j3v4gq-4000.app.github.dev/" // Took it from trailbase
 
@@ -8,8 +10,13 @@ const {dataProvider, authProvider} = await createTrailbaseProvider(TrailbaseURL)
 
 const App = () => (                                                                     // Took from src/admin/index.tsx from Marmalab
   <Admin dataProvider={dataProvider}>                                                                                               
-    <Resource name="Author" list={AuthorList} />                                  
+    <Resource name="Author" list={AuthorList} edit = {AuthorEdit} create = {AuthorCreate} show = {AuthorShow}/>                                  
   </Admin>
 );                                                                                          // Took from src/admin/index.tsx from Marmalab // change ListGuesser in {} to AuthorList
 // changed Resource name = "Author" list = {AuthorList}
+// changed edit = {EditGuesser} -> edit = {AuthorEdit}
+// put  create = {AuthorCreate}
+// put show = {ShowGuesser} this will let us view all the information of a Profile, then go to website and insect then copy code to Authr.jsx
+// change show = {ShowGuesser} -> 
+// for next edit, put edit = {EditGuesser} -> then go back to website inspect and copy editguesser code
 export default App;                                                             // Took from src/admin/index.tsx from Marmalab
--- my-admin/src/Author.jsx
+++ my-admin/src/Author.jsx
@@ -1,7 +1,11 @@
-import { DataTable, DateField, List } from 'react-admin';
+import { DataTable, DateField, List, Create } from 'react-admin';
+import { DateInput, Edit, NumberInput, SimpleForm, TextInput } from 'react-admin'; 
+import { NumberField, Show, SimpleShowLayout, TextField } from 'react-admin';
 // made this new file and copied code from website my-admin into here
+// take out all repeating imports otherwise errors like: DateField
 
-// 
+// took from inspect website, this will disapper from website after it is put in github
+// took out ID <DataTable.Col source="id" />
 export const AuthorList = () => (
     <List>
         <DataTable>
@@ -14,4 +18,47 @@ export const AuthorList = () => (
             <DataTable.NumberCol source="profileID" />
         </DataTable>
     </List>
+);
+
+
+// took from inspect website, don't touch after you copied it.
+// took out  <TextInput source="id" /> since fetch error,, can leave it in edit but not in create
+export const AuthorEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" />
+            <TextInput source="fullName" />
+            <DateInput source="dob" />
+            <TextInput source="authorBiography" />
+            <NumberInput source="profileID" />
+        </SimpleForm>
+    </Edit>
+);
+
+// have to take out <TextInput source="id" /> from AuthorCreate because will gve errors
+// copy this code from edit and just change <Edit> and </Edit> -> <Create> and </Create>
+// LEAVE THE ID out for Create because it autmaticaly increments
+export const AuthorCreate = () => (
+    <Create>
+        <SimpleForm>
+            <TextInput source="fullName" />
+            <DateInput source="dob" />
+            <TextInput source="authorBiography" />
+            <NumberInput source="profileID" />
+        </SimpleForm>
+    </Create>
+);
+
+
+// can take out id from show incase  <TextField source="id" />, asked not to show user certain fields
+// click on the website so that we can inspect
+export const AuthorShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="fullName" />
+            <DateField source="dob" />
+            <TextField source="authorBiography" />
+            <NumberField source="profileID" />
+        </SimpleShowLayout>
+    </Show>
 );
